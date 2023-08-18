708a935af9ea996db7496401c52c7c66428f00c6
leonel.llancaqueo <leonel.llancaqueo@debian-8-amd64.fi.uncoma.edu.ar>
2023-08-18 14:40:21 -0300

    estructura del proyecto

---
+++ trabajosPracticos/src/Repaso_Algoritmia/Algoritmia.java
@@ -0,0 +1,23 @@
+package Repaso_Algoritmia;
+
+public class Algoritmia {
+    //EJERCICIO1 
+
+    //EJERCICIO2 
+
+    //EJERCICIO3
+
+    //EJERCICIO4
+
+    //EJERCICIO5
+
+    //EJERCICIO6 
+
+    //EJERCICIO7 
+
+    //EJERCICIO8
+
+    //EJERCICIO9
+
+    
+}
