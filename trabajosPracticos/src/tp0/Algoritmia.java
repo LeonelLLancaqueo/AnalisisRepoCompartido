7c0e893a1c02dc460544f311e84d8565e9e4b7ea
leonel <leo87llanca@gmail.com>
2023-08-21 12:33:26 -0300

    correcion ejercicio3

--- trabajosPracticos/src/tp0/Algoritmia.java
+++ trabajosPracticos/src/tp0/Algoritmia.java
@@ -9,42 +9,44 @@ import java.util.Random;
 public class Algoritmia {
     
     //EJERCICIO1 
-    	private static boolean esPrimo(int i){
-		boolean esPrimo= true;
-		int k=2, mitadDeN= i/2;
-		
-		while(k<= mitadDeN && esPrimo){
-
-		    if(i%k == 0){
-		        esPrimo= false;
-		    }
-		    k++;
-		    
-		}
-		
-		return true;
-	    }
-	    private static int cuantosNPrimos(int n){
-		int resp=0;
-		for(int i=1; i< n; i++){
-		    if(esPrimo(i)){
-			resp++;
-		    }
-		}
-		return resp;
-	    }
-	public static void main(String[] args) {
-        //juegoAdivinar();
-
-        int n;
-        Scanner s= new Scanner(System.in); 
-        System.out.print("ingrese un numero entero: ");
-        n= s.nextInt();
-        System.out.println(cuantosNPrimos(n));
-    }
+	
     //EJERCICIO2 
 
     //EJERCICIO3
+
+		private static boolean esPrimo(int i){
+			boolean esPrimo= true;
+			int k=2, mitadDeN= i/2;
+			
+			while(k<= mitadDeN && esPrimo){
+
+				if(i%k == 0){
+					esPrimo= false;
+				}
+				k++;
+				
+			}
+			
+			return true;
+	    }
+	    private static int cuantosNPrimos(int n){
+			int resp=0;
+			for(int i=1; i< n; i++){
+				if(esPrimo(i)){
+				resp++;
+				}
+			}
+			return resp;
+	    }
+		public static void main(String[] args) {
+			//juegoAdivinar();
+
+			int n;
+			Scanner s= new Scanner(System.in); 
+			System.out.print("ingrese un numero entero: ");
+			n= s.nextInt();
+			System.out.println(cuantosNPrimos(n));
+    	}
 	
     //EJERCICIO4
 
@@ -57,6 +59,6 @@ public class Algoritmia {
     //EJERCICIO8
 
     //EJERCICIO9
-
+	
     
 }
